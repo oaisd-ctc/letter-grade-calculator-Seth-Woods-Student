89c08c2c70b727f5a52f371f92fe8c70bb0cc33a
Seth-Woods-Student <sewoae4@student.oaisd.org>
2024-10-21 13:26:13 +0000

    done

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program {
+
+
+    static void Main(string[] args) {
+       Grade();
+        }
+    public static void Grade() {
+                Console.WriteLine("what is your grade score?");
+        int GradeScore = int.Parse(Console.ReadLine());
+        if(GradeScore>100){
+            Console.WriteLine("A+");
+            Console.WriteLine("WOW!" + GradeScore + ".....tryhard!");
+        }else if(GradeScore>=90){
+            Console.WriteLine("A");
+            Console.WriteLine("Great job!!!");
+        }else if(GradeScore>=80){
+            Console.WriteLine("B");
+            Console.WriteLine("Keep it up!");
+        }else if(GradeScore>=70){
+            Console.WriteLine("C");
+            Console.WriteLine("Keep trying!");
+        }else if(GradeScore>=60){
+            Console.WriteLine("D");
+            Console.WriteLine("You've got this!");
+        }else{
+            Console.WriteLine("F");
+            Console.WriteLine("You FAILED! GET GOOD NOOB!!!!");
+    }
+    }
+}
